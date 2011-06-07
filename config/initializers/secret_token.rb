16b808aa83020aee4b8f369c1d6ec4c9d104c51b
sdennler <den@vollbio.de>
2011-06-07 17:03:22 +0545

    Rails Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelLog::Application.config.secret_token = '05817a44cb1ac720fbe91e4e5ffb97581e92d90b7d198b8dd29ca1ef8f19b7bcb1efe5473ff95dadc29ab73b77c10039abe7225d4eef66dd5b764d3005d3d625'
