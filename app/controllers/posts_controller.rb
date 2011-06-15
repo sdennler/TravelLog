65dd81d02edd8c9e7e1fbfac0e05889122d3a85c
sdennler <den@vollbio.de>
2011-06-15 10:11:26 +0545

    Bubles back in Layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,20 +5,14 @@ class PostsController < ApplicationController
     @posts = Post.order('date DESC').all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html { render '_index' }
       format.xml  { render :xml => @posts }
     end
   end
 
-  # GET /posts/1
-  # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
+    @posts = Post.order('date DESC').all
   end
 
   # GET /posts/new
