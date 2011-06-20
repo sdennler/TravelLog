02d00ea9e4f57ce992777ff3a205719ff31e573a
sdennler <den@vollbio.de>
2011-06-21 01:05:57 +0545

    helper show_title_for

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+
+ def show_title_for post
+  (post.title.nil? or post.title.empty?) ? '' : ' - ' + post.title
+ end
+
 end
--- test/unit/helpers/posts_helper_test.rb
+++ test/unit/helpers/posts_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class PostsHelperTest < ActionView::TestCase
+
+ test "show_title_for should give hypen and the title" do
+  assert_equal ' - '+posts(:whit_title).title, show_title_for(posts(:whit_title))
+ end
+
+ test "show_title_for should give empty string" do
+  assert_equal '', show_title_for(posts(:only_location))
+ end
+
 end
